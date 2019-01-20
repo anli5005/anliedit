456d8c66973a39adf061df494ea05774f041fc6c
Anthony Li <li.anthony917@gmail.com>
2019-01-20 17:15:28 -0500

    Fix Google Drive

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  GOOGLE_CLIENT_ID: '"241271498917-t4t7d07qis7oc0ahaskbif3ft6tk63cd.apps.googleusercontent.com"',
+  GOOGLE_CLIENT_ID: '"329294602126-qv838uqpjhavb9om6r8o6bephln4p7en.apps.googleusercontent.com"',
   GITHUB_CLIENT_ID: '"none"'
 }
