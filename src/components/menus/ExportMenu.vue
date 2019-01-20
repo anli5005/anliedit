62f0b028bb9efaf6d0c96dc27533debe4a66725a
Anthony Li <li.anthony917@gmail.com>
2019-01-19 23:16:30 -0500

    Remove sponsored export options

--- src/components/menus/ExportMenu.vue
+++ src/components/menus/ExportMenu.vue
@@ -10,16 +10,6 @@
       <div>Export as HTML</div>
       <span>Generate an HTML page from a template.</span>
     </menu-entry>
-    <menu-entry @click.native="exportPdf">
-      <icon-download slot="icon"></icon-download>
-      <div><div class="menu-entry__label" :class="{'menu-entry__label--warning': !isSponsor}">sponsor</div> Export as PDF</div>
-      <span>Produce a PDF from an HTML template.</span>
-    </menu-entry>
-    <menu-entry @click.native="exportPandoc">
-      <icon-download slot="icon"></icon-download>
-      <div><div class="menu-entry__label" :class="{'menu-entry__label--warning': !isSponsor}">sponsor</div> Export with Pandoc</div>
-      <span>Convert to PDF, Word, EPUB...</span>
-    </menu-entry>
   </div>
 </template>
 
